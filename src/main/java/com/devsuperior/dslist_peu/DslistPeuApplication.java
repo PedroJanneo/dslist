6e6d8f27f2fb27aa2fdaca390fc5db3ab5ea8a98
Pedro Janneo <pdro.henriquejanneo@gmail.com>
2025-08-18 11:31:31 -0300

    Projeto criado

---
+++ src/main/java/com/devsuperior/dslist_peu/DslistPeuApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist_peu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistPeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistPeuApplication.class, args);
+	}
+
+}
